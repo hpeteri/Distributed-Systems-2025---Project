3fc90638e8ca9eaf10a665e6e57b194fb1ad49d0
Aleksanteri Seppa <aseppa20@student.oulu.fi>
2025-01-20 16:06:11 +0200

    Client now plays a test video

--- web_client/web_server.py
+++ web_client/web_server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, StreamingResponse
+from pathlib import Path
 
 app = FastAPI(title="Web Client for Video Streaming")
 
@@ -15,12 +16,17 @@ def generate_html_response():
         </body>
     </html>
     """
-    return HTMLResponse(content=html_content, status_code=200)
+    return HTMLResponse(content=html_content, status_code=200)   
 
-
-@app.get("/video/", response_class=HTMLResponse)
-async def read_items():
-    return generate_html_response()
+@app.get("/video", response_class=StreamingResponse)
+def video_endpoint():
+    def stream():
+        #video_path = "C:\\Users\\Allu\\Desktop\\Ohjelmointi\\DS\\ex1\\Distributed-Systems-2025---Project\\web_client\\vids\\testvid.mp4"
+        video_path = Path("/vids/testvid.mp4")
+        with open(video_path, mode="rb") as file_like:
+            yield from file_like
+        
+    return StreamingResponse(stream(), media_type="video/mp4", status_code=206)
 
 app.mount("/", StaticFiles(directory="www", html=True), name="Main Site")
-app.mount("/video/", read_items())
+app.mount("/video", video_endpoint())
